6ae9a958a1135e5a44abf50a0f05d98b5c5c8227
gustavo <kg.2k.pl@gmail.com>
2025-06-05 22:48:11 -0400

    feat(db): add database connection with Sequelize
    - Implemented database connection using Sequelize in server.ts
    - Added dotenv for environment variable management
    - Updated package.json with new dependencies: dotenv, pg, pg-hstore, and sequelize

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,18 @@
 import express from 'express'
 import router from './router'
+import db from './config/db'
+
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log('Connected to the database successfully')
+    } catch (error) {
+        console.error(error)
+        console.log('Failed to connect to the database')
+    }
+}
+connectDB()
 
 const server = express()
 
