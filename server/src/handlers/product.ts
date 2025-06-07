03ff79fcf120a11786827e5fe138b544f006e8b3
gustavo <kg.2k.pl@gmail.com>
2025-06-07 07:07:58 -0400

    feat(product): add validation for product creation and update dependencies

--- server/src/handlers/product.ts
+++ server/src/handlers/product.ts
@@ -1,8 +1,23 @@
-import { Request, Response } from 'express';
-import Product from '../models/Product.model';
+import { Request, Response } from 'express'
+import { check, validationResult } from 'express-validator'
+import Product from '../models/Product.model'
 
 export const createProduct = async (req: Request, res: Response) => {
-    
+
+    await check('name')
+        .notEmpty().withMessage('El nombre es obligatorio')
+        .run(req)
+    await check('price')
+        .isNumeric().withMessage('El precio debe ser un número')
+        .notEmpty().withMessage('El precio es obligatorio')
+        .custom(value => value > 0).withMessage('El precio debe ser mayor que cero')
+        .run(req)
+
+    let errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
     const product = await Product.create(req.body)
-    res.json({data: product})
+    res.json({ data: product })
 }   
