b0e4d7ab2597457248a916d8bdd2bd51f7e42246
gustavo <kg.2k.pl@gmail.com>
2025-06-06 21:30:39 -0400

    feat(models): add Product model with Sequelize and update dependencies

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,16 @@
 import express from 'express'
 import router from './router'
 import db from './config/db'
+import colors from 'colors'
 
 async function connectDB() {
     try {
         await db.authenticate()
         db.sync()
-        console.log('Connected to the database successfully')
+        console.log(colors.magenta.bold('Connected to the database successfully'))
     } catch (error) {
         console.error(error)
-        console.log('Failed to connect to the database')
+        console.log(colors.bgRed.bold('Failed to connect to the database'))
     }
 }
 connectDB()
