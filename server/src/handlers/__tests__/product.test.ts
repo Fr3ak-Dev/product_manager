32329b7084cf4495b5a38f044ab2dd31296d8a22
gustavo <kg.2k.pl@gmail.com>
2025-06-07 21:12:36 -0400

    test(product): add validation tests for price constraints in product creation

--- server/src/handlers/__tests__/product.test.ts
+++ server/src/handlers/__tests__/product.test.ts
@@ -13,6 +13,34 @@ describe('POST /api/products', () => {
         expect(response.body.errors).not.toHaveLength(2)
     })
 
+    it('should validate that the price is greather than 0', async () => {
+        const response = await request(server).post('/api/products').send({
+            name: 'Test Product',
+            price: -5,
+        })
+
+        expect(response.statusCode).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+
+    it('should validate that the price is a number and greather than 0', async () => {
+        const response = await request(server).post('/api/products').send({
+            name: 'Test Product',
+            price: -5,
+        })
+
+        expect(response.statusCode).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(2)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(4)
+    })
+
     it('should create a new product', async () => {
         const response = await request(server).post('/api/products').send({
             name: 'Test Product',
