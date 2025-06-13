1f0abf14db94607414f3c58eebfde9c72ee9c10f
gustavo <kg.2k.pl@gmail.com>
2025-06-12 22:29:32 -0400

    feat(client): add form handling and action for NewProduct component - Actions React Router

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./Layouts/Layout";
 import Products from "./views/Products";
-import NewProduct from "./views/Newproduct";
+import NewProduct, { action as NewProductAction } from "./views/Newproduct";
 
 export const router = createBrowserRouter([
     {
@@ -14,7 +14,8 @@ export const router = createBrowserRouter([
             },
             {
                 path: 'products/new',
-                element: <NewProduct />
+                element: <NewProduct />,
+                action: NewProductAction
             }
         ]
     }
