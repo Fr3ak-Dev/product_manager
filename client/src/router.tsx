c248ac8d351ef1fa56b4a3db68733898c1e92f11
gustavo <kg.2k.pl@gmail.com>
2025-06-17 20:45:08 -0400

    feat(client): enhance product management by adding product loader and schemas - loader react router

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./Layouts/Layout";
-import Products from "./views/Products";
-import NewProduct, { action as NewProductAction } from "./views/Newproduct";
+import Products, { loader as productsLoader } from "./views/Products";
+import NewProduct, { action as NewProductAction } from "./views/NewProduct";
 
 export const router = createBrowserRouter([
     {
@@ -10,7 +10,8 @@ export const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <Products />
+                element: <Products />,
+                loader: productsLoader
             },
             {
                 path: 'products/new',
